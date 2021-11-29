8c2356b300a1662a716287e76e5be3437c920003
MFatigati <fatigati.michael@gmail.com>
2021-11-29 15:55:32 -0500

    basic browser to app to database connections established

--- texts_app.js
+++ texts_app.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const path = require('path');
+const DBQUERY = require('./lib/dbQuery')
 
 const app = express();
 const host = "localhost";
 const port = 3000;
+
+SQL = "SELECT * FROM texts";
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/test", (req, res) => {
+  res.sendFile('/views/test.html', {root: __dirname})
+});
+
+app.get("/database", (req, res, next) => {
+  DBQUERY.connectToDB()
+    .then((data) => { return DBQUERY.getResult(SQL, data)})
+    .then((data) => { return DBQUERY.disconnectFromDB(data)})
+    .then((data) => {
+      console.log(data.result.rows);
+      res.json(data.result.rows);
+      return data;
+    })
+    .catch(err => console.log(err))
+})
 
 app.listen(port, host, () => {
   console.log(`Texts project is listening on port ${port} of ${host}!`);
